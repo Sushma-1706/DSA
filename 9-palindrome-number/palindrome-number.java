bd449d350af299be0f5224a1c2e0ca9a2ec8bd99
DAMACHARLA SUSHMA <damacharlasushma@gmail.com>
2025-12-29 17:03:01 +0530

    Time: 5 ms (73.95%) | Memory: 46 MB (48.96%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int val =x;
+        int rev = 0;
+        while(x>0){
+          rev = rev*10+x%10;
+          x/=10;
+         
+        }
+         return rev == val;
+    }
+        // if(isPalindrome(x)){
+        //     System.out.println("true");
+        // }else{
+        //     System.out.println("false");
+        // }
+    
+}
